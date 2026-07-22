8f8cc36eafee8b4995d8374d1a55139dcdd42076
Bruno Pinho <bruno.pinho@linde.com>
2026-07-22 10:26:07 -0300

    refactor(mason): merge tool list into mason opts.ensure_installed
    Remove mason-tool-installer.nvim and let LazyVim manage a single merged
    install queue to fix 'Package is already installing' race error.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,32 +1,25 @@
 return {
   {
     "mason-org/mason.nvim",
-    opts = {},
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = { "mason-org/mason.nvim" },
-    config = function()
-      require("mason-tool-installer").setup({
-        ensure_installed = {
-          -- LSPs
-          "pyright",
-          "lua-language-server",
-          "json-lsp",
-          "marksman",
-          "rust-analyzer",
-          "taplo",
-          -- DAP
-          "codelldb",
-          "debugpy",
-          -- Formatters/linters
-          "ruff",
-          "stylua",
-          "shfmt",
-          "markdownlint-cli2",
-          "markdown-toc",
-        },
-      })
-    end,
+    opts = {
+      ensure_installed = {
+        -- LSPs
+        "pyright",
+        "lua-language-server",
+        "json-lsp",
+        "marksman",
+        "rust-analyzer",
+        "taplo",
+        -- DAP
+        "codelldb",
+        "debugpy",
+        -- Formatters/linters
+        "ruff",
+        "stylua",
+        "shfmt",
+        "markdownlint-cli2",
+        "markdown-toc",
+      },
+    },
   },
 }
